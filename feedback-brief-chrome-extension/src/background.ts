a9d7a1cd6ddd6c833186b87ee50983e632db1592
robincheng <robincheng09@gmail.com>
2025-02-04 13:10:00 +0800

    create background.ts

--- feedback-brief-chrome-extension/src/background.ts
+++ feedback-brief-chrome-extension/src/background.ts
@@ -1,7 +1,7 @@
 chrome.runtime.onInstalled.addListener(() => {
     chrome.contextMenus.create({
         id: "send-highlighted-text",
-        title: "Send highlighted text",
+        title: "Send highlighted text to Feedback Brief",
         contexts: ["selection"]
     })
 })
