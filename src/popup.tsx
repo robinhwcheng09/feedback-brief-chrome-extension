7b1caafe8623a4f9adda27116aae86091e699002
robincheng <robincheng09@gmail.com>
2025-03-10 14:10:06 +0800

    add sync host

--- src/popup.tsx
+++ src/popup.tsx
@@ -10,10 +10,11 @@ import React from "react"
 import '~style.css'
 
 const PUBLISHABLE_KEY = process.env.PLASMO_PUBLIC_CLERK_PUBLISHABLE_KEY
+const SYNC_HOST = process.env.PLASMO_PUBLIC_CLERK_SYNC_HOST
 const EXTENSION_URL = chrome.runtime.getURL('.')
 
-if (!PUBLISHABLE_KEY) {
-  throw new Error('Please add the PLASMO_PUBLIC_CLERK_PUBLISHABLE_KEY to the .env.development file')
+if (!PUBLISHABLE_KEY || !SYNC_HOST) {
+  throw new Error('Please add the PLASMO_PUBLIC_CLERK_PUBLISHABLE_KEY and PLASMO_PUBLIC_CLERK_SYNC_HOST to the .env.development file')
 }
 
 function IndexPopup() {
@@ -41,6 +42,7 @@ function IndexPopup() {
       afterSignOutUrl={`${EXTENSION_URL}/popup.html`}
       signInFallbackRedirectUrl={`${EXTENSION_URL}/popup.html`}
       signUpFallbackRedirectUrl={`${EXTENSION_URL}/popup.html`}
+      syncHost={SYNC_HOST}
     >
       <div className="plasmo-flex plasmo-items-center plasmo-justify-center plasmo-h-[600px] plasmo-w-[800px] plasmo-flex-col">
         <header className="plasmo-w-full">
